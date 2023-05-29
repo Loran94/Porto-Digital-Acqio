ee63c8e2d44d60ee3215b47009962f8be3deb885
Matheus Loran <111586873+Loran94@users.noreply.github.com>
2023-05-29 11:20:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,29 @@ function start() {
 }
 
 window.addEventListener("load", start)
+
+
+//Likes
+var botoes = document.querySelectorAll(".like");
+
+		for (var i = 0; i < botoes.length; i++) {
+			botoes[i].addEventListener("click", function() {
+				var id = this.id;
+				if (!localStorage.getItem(id)) {
+					localStorage.setItem(id, 0);
+				} else {
+					var count = parseInt(localStorage.getItem(id));
+					count = 1;
+					localStorage.setItem(id, count);
+				}
+				alert("Botão " + id + " clicado " + localStorage.getItem(id) + " vezes.");
+			}
+      )
+    }
+
+// Botao evniar
+function enviarFormulario() {
+		var nome = document.getElementById("#nome").value;
+		window.location.href = "#" + encodeURIComponent(nome);
+        console.log(enviarFormulario);
+}
